a7d18bab37827caba3bb8530e92f377e03fc790e
GovindMore <govind.more_24uml@sanjivani.edu.in>
2024-12-29 23:31:03 -0800

    done

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Print the welcome message to the console
+        System.out.println("Welcome tothe CI/CD Pipeline Practical!");
+    }
+}
+
